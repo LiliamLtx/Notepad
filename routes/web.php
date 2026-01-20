e4fe21121898559fcbd6cacfbab0b0c2e3e5f471
Liliam Teixeira <liliam.silvateixeira@gmail.com>
2026-01-20 11:33:23 -0300

    Adicionando rota de cadastro

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([CheckIsNotLogged::class])->group(function(){
     Route::get('/login', [AuthController::class, 'login']);
     Route::post('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('loginSubmit');
+    Route::get('/cadastro', [AuthController::class, 'cadastro'])->name('cadastro');
+    Route::post('/cadastroSubmit', [AuthController::class, 'cadastroSubmit'])->name('cadastroSubmit');
 });
 
-//app route
+//app route -- user logged
 Route::middleware([CheckIsLogged::class])->group(function(){
     //user logado
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/', [MainController::class, 'index'])->name('home');
+
+    //notes
+    Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
     Route::get('/newNote',[MainController::class, 'newNote'])->name('new');
+    //edit not
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+    Route::post('/editNoteSubmit',[MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
+
+    //delet note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete'); 
+    Route::get('/deleteNoteConfirm/{id}', [MainController::class, 'deleteNoteConfirm'])->name('deleteConfirm');
 });
 
 
