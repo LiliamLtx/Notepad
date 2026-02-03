150465e99d3638e6ea1f735d281504e61fd1c042
Liliam Teixeira <liliam.silvateixeira@gmail.com>
2026-02-03 15:07:22 -0300

    E2E Cypress: login

---
+++ frontend/cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+describe('Login', () => {
+    //Arrange
+    beforeEach(() => {
+        cy.visit('/login')
+    })
+
+    it('Realizar login com sucesso', () =>{
+        //Act
+        cy.get('[data-test="username"]').type('Testandoo@gmail.com')
+        cy.get('[data-test="password"]').type('123456')
+        cy.get('[data-test="login"]').click()
+        //Assert
+        cy.url().should('include', '/home')
+        cy.get('[data-test="TopBar"]')
+        .should('be.visible')
+        .and('exist')
+    })
+
+    it('Realizar login com credenciais erradas', () =>{
+
+        //Act
+        cy.get('[data-test="username"]').type('errando@gmail.com')
+        cy.get('[data-test="password"]').type('errada')
+        cy.get('[data-test="login"]').click()
+        //Assert
+        cy.get('[data-test="error"]').should('contain.text', 'Credenciais inválidas')
+        
+        
+    } )
+})
