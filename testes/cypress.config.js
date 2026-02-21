c54a029480bfddef16ff09131a633155bb5e6670
Liliam Teixeira <liliam.silvateixeira@gmail.com>
2026-02-21 12:47:51 -0300

    Fix: resolvendo problema de componente

--- testes/cypress.config.js
+++ testes/cypress.config.js
@@ -4,12 +4,13 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173/',
+    supportFile: false,
   },
-  component: {
+  /*component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
     devServer: {
       framework: 'vue',
       bundler: 'vite',
     },
-  },
+  },*/
 })
